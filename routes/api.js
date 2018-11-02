1026fb4a52c09b9181ef445bcc8676e4b639d534
Matthew Amis <mfamis@gmail.com>
2018-11-01 18:14:07 -0700

    Adding functional products from collections route.
    Debug prints still in code, will remove prior to merge.

--- routes/api.js
+++ routes/api.js
@@ -5,15 +5,60 @@ router.route("/collections").get((req, res) => {
 
 });
 
+function getCollectionId(collectionName) {
+  let url = `${process.env.SHOPIFY_URL}/admin/custom_collections.json?title=${collectionName}`;
+  return new Promise((resolve, reject) => {
+    axios.get(url).then((response) => {
+      if (response.data.custom_collections.length === 1) {
+        resolve(response.data.custom_collections[0].id);
+      }
+      else {
+        reject("Collection supplied is not specific enough");
+      }
+    });
+  });
+}
+
+function getCollectsInCollection(collectionId) {
+  let url = `${process.env.SHOPIFY_URL}/admin/collects.json?collection_id=${collectionId}`;
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(response => resolve(response.data.collects))
+      .catch(error => reject(error));
+  });
+}
+
+function getProductsFromCollects(collects) {
+  return new Promise((resolve, reject) => {
+    let productsCsv = collects.map(collect => collect.product_id).join(",");
+    let url = `${process.env.SHOPIFY_URL}/admin/products.json?ids=${productsCsv}`;
+    console.log(productsCsv);
+    console.log(url);
+    axios.get(url)
+      .then(response => resolve(response.data.products))
+      .catch(error => reject(error));
+  });
+}
+
 router.route("/products/:collection").get((req, res) => {
-  let url = `${process.env.SHOPIFY_URL}/admin/custom_collections.json?title=${req.params.collection}`;
-  console.log(url);
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(".then:");
-      console.log(response.data);
-      res.send(response.data);
+  getCollectionId(req.params.collection)
+    .then((id) => {
+      console.log(`id: ${id}`);
+      getCollectsInCollection(id)
+      .then((collects) => {
+        console.log(`collects: `);
+        collects.map(collect => {
+          console.log(collect.id);
+        });
+        getProductsFromCollects(collects)
+        .then(products => {
+          console.log(`products: `);
+          products.map(product => {
+            console.log(product.id);
+          });
+          res.json(products);
+        })
+      });
     })
     .catch((error) => {
       console.log(".catch:");
