ce9c4fb536d8ef196588a22abe42e2d2d5159537
Nguyen Le <ucnguyenle@gmail.com>
2018-11-02 20:32:09 -0700

    add questions with grids

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,17 @@ import Button from '@material-ui/core/Button';
 import QuestMain from './components/QuestMain'
 import Header from './components/Header'
 import GetStarted from './components/GetStarted'
+import Questionaire from './components/Questionaire'
 
 function App() {
   return (
     <div>
     <Header />
     <GetStarted />
+    <br />
+    <br />
+    <Questionaire />
+
     <QuestMain />
     </div>
   );
---
+++ client/src/components/Questionaire/Questionaire.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    height: 200,
+  },
+});
+
+function FormRow(props) {
+  const { classes } = props;
+
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>item 1</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>item 2</Paper>
+      </Grid>
+      <Grid item xs={4}>
+        <Paper className={classes.paper}>item 3</Paper>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+FormRow.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+function Questionaire(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={8}>
+        <Grid container item xs={12} spacing={24}>
+          <FormRow classes={classes} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+Questionaire.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Questionaire);
