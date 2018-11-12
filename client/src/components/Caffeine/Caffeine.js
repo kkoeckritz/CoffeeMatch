e1dab34ba233f8a6b7875c900322233ed7843507
Nguyen Le <ucnguyenle@gmail.com>
2018-11-11 21:37:31 -0800

    attempt at getting collections to show on caffeine page

--- client/src/components/Caffeine/Caffeine.js
+++ client/src/components/Caffeine/Caffeine.js
@@ -1,19 +1,61 @@
 import React, {Component} from "react";
 import "./Caffeine.css";
+// import axios from 'axios';
 
 class Caffeine extends Component { 
-    render() {
-        return (
-    <div className="caffeine">
-    <p>Now then, do you prefer familiar flavors or something a bit more adventurous? Choose a collection below.</p>
-    <p>Caffeine stuff will appear here with different choices
-        </p>
-        <p>map the different caffeine choices</p>
-        
-    </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          collections: []
+        };
+      }
     
-        )
+      componentDidMount() {
+        fetch("/api/collections")
+          .then(res => res.json())
+          .then(
+            (result) => {
+                console.log(result);
+              this.setState({
+                isLoaded: true,
+                collections: result.collections
+              });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+          )
+      }
+    
+      render() {
+        const { error, isLoaded, collections } = this.state;
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
+          return (
+              <div>
+            
+              {collections.map(collection => (
+                  
+                <h1>
+                  {collection.name}
+                </h1>
+              ))}
+            
+            </div>
+          );
+        }
+      }
     }
-}
 
 export default Caffeine;
