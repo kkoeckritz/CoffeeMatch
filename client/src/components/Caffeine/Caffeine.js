2a52540f588ed105cc2b4247cf6cbf0e5d322e30
Nguyen Le <ucnguyenle@gmail.com>
2018-11-12 14:27:05 -0800

    pulled data from api/collections

--- client/src/components/Caffeine/Caffeine.js
+++ client/src/components/Caffeine/Caffeine.js
@@ -1,73 +1,45 @@
 import React, {Component} from "react";
 import "./Caffeine.css";
-import axios from 'axios';
+// import axios from 'axios';
 
 class Caffeine extends Component { 
-    constructor(props) {
-        super(props);
-        this.state = {
-          error: null,
-          questionText: "Now then, do you prefer familiar flavors or something a bit more adventurous? Choose a collection below.",
-          isLoaded: false,
-          collections: [],
-          answersFunction: () => {
-            axios.get("/api/collections").then((collections) => {
-              return collections.map(collection => {
-                return {
-                  text: collection.name,
-                  value: collection.handle,
-                }
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: []
+    };
+  }
 
-              }
-            )
-            });
-          }
-        
+  componentDidMount() {
     
+    fetch("/api/collections")
+      .then(results => {
+        return results.json();
+      }).then(data => {
+        let collections = data.map((collection) => {
+          return(
+            <div key={collection.results}>
+              <p>{collection.name}</p>
 
-        };
-      }
-    
-      componentDidMount() {
-        fetch("/api/collections")
-          .then(res => res.json())
-          .then(
-            (result) => {
-                console.log(result);
-              this.setState({
-                isLoaded: true,
-                collections: result.collections
-              });
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
+              </div>
           )
-      }
+        })
+        this.setState({collections: collections});
+        console.log("state", this.state.collections);
 
-      componentWillRecieveProps(nextProps) {
-        
       }
-    
-      render() {
+    )
         
-          return (
-              <div className="caffeine">
-                <p>{ this.state.questionText } </p>
-            
-          
-             
-            
-            </div>
-          );
-        
-      }
+       
+  }
+
+  render() {
+      return(
+        <div className="caffeine">
+            <p>{this.state.collections}</p>
+        </div>
+      )
+    }
     }
 
 export default Caffeine;
