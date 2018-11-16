dcb498dfd0f50b701ac46d23d83a44c1173d2399
whitbrooks <whitbrooks@gmail.com>
2018-11-16 14:28:08 -0800

    adding new questions

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
       choices: ["Decaf", "Caffeinated"],
       info: ["Description of decaf", "Description of Caffeine"],
       text: {
-        question1: "First things first: Regular or Decaf?",
-        question2: "Now then, do you prefer familiar flavors or something a bit more adventurous?",
-        question3: "You're almost done! Which flavor profile most appeals to you?",
-        result: "You made it! Take a look at these matches!"
+        question1: "Looking to cozy up and chill, or want to get your blood pumping? Ehem, we mean, do you want decaf or caffeinated coffee?!",
+        question2: "Now then, do you prefer something casual, or perhaps a bit more adventurous? For real, get your mind out of the gutter.",
+        question3: "Great, now that your mind's back on coffee, which of these flavors to you find most appealing?",
+        result: "Yay! Here are your potential SoulMates. Enjoy your first date ;) "
       },
       caffeine: "caffeine",
       collectionHandle: "none",
--- client/src/components/GetStarted/GetStarted.js
+++ client/src/components/GetStarted/GetStarted.js
@@ -8,13 +8,13 @@ class GetStarted extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <h5 className="getStarted">Not sure where to start?
-        Let us help you discover new flavors, or find a favorite.
+        <h5 className="getStarted">Not sure where to start?	Let us help you find your SoulMate in three easy steps! No… not your human soul mate. You're on your own with that. We're not miracle workers, we just make great coffee.
+
             </h5>
             <br/>
         <BrowserRouter>
           <Switch>
-            <a href="/home" className="waves-effect waves-light btn-large red darken-3">Let's Get Started!</a>
+            <a href="/home" className="waves-effect waves-light btn-large red darken-3">find your "SoulMate"</a>
          {/* <Link to='/home'> <img src={logo} className="App-logo" alt="logo" />
         </Link> */}
         </Switch>
