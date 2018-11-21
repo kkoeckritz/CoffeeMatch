ccd7c8082d7fbbbedbf22e0f6ffa489cd4f8971e
Kris Koeckritz <kjk464@humboldt.edu>
2018-11-20 18:28:09 -0800

    clean up main.js

--- client/public/assets/js/main.js
+++ client/public/assets/js/main.js
@@ -1,16 +1,7 @@
+/* eslint-disable */
 $(document).ready(function() {
   $('.sidenav').sidenav();
   $('.tabs').tabs();
   $('.fixed-action-btn').floatingActionButton();
-  // $('.tooltipped').tooltip();
-   function go() {
-    const fileInput = document.getElementById('file');
-    const outputDiv = document.getElementById('output');
-    let html = '';
-    for (const file of fileInput.files) {
-      html += '<img src="images/' + file.name + '">';
-    }
-    outputDiv.textContent = html;
-  }
-  
+  /* eslint-enable */
 });
