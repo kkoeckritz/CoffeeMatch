cb7172ebd02cdf9823ece9f050bedb3e925dda3e
Matthew Amis <mfamis@gmail.com>
2018-11-24 14:36:13 -0800

    Adding documentation for decaf update task script.

--- db/decaf_update_task.js
+++ db/decaf_update_task.js
@@ -8,6 +8,12 @@ mongoose.connect(
   "mongodb://localhost/coffeematch"
 );
 
+/**
+ * handleGetCollectsErrors - Ignores "No collects found!" error, but rejects all other errors
+ * @param {function} resolve - Resolve function in parent promise
+ * @param {function} reject - Reject function in parent promise
+ * @param {string} error - Error message received
+ */
 var handleGetCollectsErrors = (resolve, reject, error) => {
   if (error.includes("No collects found!")) { // ignore no collects found
     resolve(error);
@@ -16,19 +22,31 @@ var handleGetCollectsErrors = (resolve, reject, error) => {
   }
 };
 
+/**
+ * Iterates over supplied products and determines if any product is decaf
+ * @param {array} products - array of product objects
+ * @returns Boolean of whether or not there are decaf products
+ */
 var anyProductsDecaf = (products) => {
   return products.filter(product => (
     product.tags.toLowerCase().includes('decaf')
   )).length > 0;
 };
 
-var enqueueAsyncCollectionDecafUpdates = (collections) => (
+/**
+ * Iterates over collection objects, checks if they have decaf coffees, 
+ * and updates the database record appropriately. This is done asyncronously,
+ * so this function returns a list of promises that needs to be handled.
+ * @param {array} collections - array of collection objects from database
+ * @returns Array of promises
+ */
+var asyncUpdateCollectionDecaf = (collections) => (
   collections.map(collection => {
     return new Promise((resolve, reject) => {
       shopify.getCollectionIdFromName(collection.handle).then(id => {
         shopify.getCollectsInCollection(id).then(collects => {
           shopify.getProductsFromCollects(collects).then(products => {
-            console.log(products);
+            console.log(products.map(p => `${p.title}: ${p.tags}`));
             collection.has_decaf = anyProductsDecaf(products);
             console.log(collection.has_decaf);
             db.Collection
@@ -42,26 +60,13 @@ var enqueueAsyncCollectionDecafUpdates = (collections) => (
   }) // collections
 )
 
+/**
+ * Get all collections from database, asyncronously update the decaf
+ * flag for each collection. Wait until all tasks are complete and exit.
+ */
 db.Collection.find({})
   .then(collections => {
-    let promises = collections.map(collection => {
-      return new Promise((resolve, reject) => {
-        shopify.getCollectionIdFromName(collection.handle).then(id => {
-          shopify.getCollectsInCollection(id).then(collects => {
-            shopify.getProductsFromCollects(collects).then(products => {
-              console.log(products.map(p => `${p.title}: ${p.tags}`));
-              collection.has_decaf = anyProductsDecaf(products);
-              console.log(collection.has_decaf);
-              db.Collection
-                .findOneAndUpdate({ _id: collection._id }, collection)
-                .then(response => resolve(response))
-                .catch(error => reject(error));
-            }).catch(error => reject(error)); // products lookup
-          }).catch(error =>  handleGetCollectsErrors(resolve, reject, error)); // collects lookup
-        }).catch(error => reject(error)); // collection lookup
-      }); // promises
-    }); // collections
-    
+    let promises = asyncUpdateCollectionDecaf(collections);
     Promise.all(promises)
       .then(response => {
         console.log("Tasks completed successfully, exiting.");
