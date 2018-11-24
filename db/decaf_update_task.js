d4742d5c70854dfbf0202cd81c72ebc06b34cacc
Matthew Amis <mfamis@gmail.com>
2018-11-24 14:14:23 -0800

    More cleanup.

--- db/decaf_update_task.js
+++ db/decaf_update_task.js
@@ -8,35 +8,58 @@ mongoose.connect(
   "mongodb://localhost/coffeematch"
 );
 
+var handleGetCollectsErrors = (resolve, reject, error) => {
+  if (error.includes("No collects found!")) { // ignore no collects found
+    resolve(error);
+  } else { 
+    reject(error)
+  }
+};
+
+var anyProductsDecaf = (products) => {
+  return products.filter(product => (
+    product.tags.toLowerCase().includes('decaf')
+  )).length > 0;
+};
+
+var enqueueAsyncCollectionDecafUpdates = (collections) => (
+  collections.map(collection => {
+    return new Promise((resolve, reject) => {
+      shopify.getCollectionIdFromName(collection.handle).then(id => {
+        shopify.getCollectsInCollection(id).then(collects => {
+          shopify.getProductsFromCollects(collects).then(products => {
+            console.log(products);
+            collection.has_decaf = anyProductsDecaf(products);
+            console.log(collection.has_decaf);
+            db.Collection
+              .findOneAndUpdate({ _id: collection._id }, collection)
+              .then(response => resolve(response))
+              .catch(error => reject(error));
+          }).catch(error => reject(error)); // products lookup
+        }).catch(error =>  handleGetCollectsErrors(resolve, reject, error)); // collects lookup
+      }).catch(error => reject(error)); // collection lookup
+    }); // promises
+  }) // collections
+)
+
 db.Collection.find({})
   .then(collections => {
-    let promises = [];
-    console.log(collections);
-    collections.map(collection => {
-      promises.push(new Promise((resolve, reject) => {
+    let promises = collections.map(collection => {
+      return new Promise((resolve, reject) => {
         shopify.getCollectionIdFromName(collection.handle).then(id => {
           shopify.getCollectsInCollection(id).then(collects => {
             shopify.getProductsFromCollects(collects).then(products => {
-              let decafProducts = products.filter(product => {
-                return product.tags.toLowerCase().includes('decaf');
-              });
-              collection.has_decaf = decafProducts.length > 0;
-
+              console.log(products.map(p => `${p.title}: ${p.tags}`));
+              collection.has_decaf = anyProductsDecaf(products);
+              console.log(collection.has_decaf);
               db.Collection
                 .findOneAndUpdate({ _id: collection._id }, collection)
                 .then(response => resolve(response))
                 .catch(error => reject(error));
-            }).catch(error => reject(error));
-          })
-          .catch(error => {
-            if (error.includes("No collects found!")) {
-              resolve(error);
-            } else {
-              reject(error)
-            }
-          }); // collects lookup
+            }).catch(error => reject(error)); // products lookup
+          }).catch(error =>  handleGetCollectsErrors(resolve, reject, error)); // collects lookup
         }).catch(error => reject(error)); // collection lookup
-      })); // promises
+      }); // promises
     }); // collections
     
     Promise.all(promises)
@@ -49,7 +72,7 @@ db.Collection.find({})
         console.log(error);
         process.exit(1);
       });
-  })
+  }) // db.Collection.find({}), find all collections
   .catch(err => {
     console.error(err);
     process.exit(1);
